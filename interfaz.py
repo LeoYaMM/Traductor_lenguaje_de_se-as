3c1b9dbeba4c69abac1949b76722f5db6fe54fe5
Aldo Escamilla Resendiz <resendizae318@gmail.com>
2024-01-13 14:49:39 -0600

    detalles en interfaz tk

--- interfaz.py
+++ interfaz.py
@@ -5,30 +5,37 @@ def login():
     username = entry_username.get()
     password = entry_password.get()
 
-    if username == "usuario" and password == "contraseña":  # Reemplazar con la lógica de autenticación real
+    if username == "usuario" and password == "contraseña": 
         clear_gui()
         show_camera_button()
     else:
         messagebox.showerror("Error", "Credenciales incorrectas")
 
 def clear_gui():
-    entry_username.delete(0, tk.END)
-    entry_password.delete(0, tk.END)
-    entry_username.pack_forget()
-    entry_password.pack_forget()
-    button_login.pack_forget()
+    login_frame.pack_forget()
+    
+canvas_width = 800
+canvas_height = 600
+rect_width = 400
+rect_height = 300
+center_x = canvas_width / 2
+center_y = canvas_height / 2
+x1 = center_x - rect_width / 2
+y1 = center_y - rect_height / 2
+x2 = center_x + rect_width / 2
+y2 = center_y + rect_height / 2
+
 
 def show_camera_button():
-    rect = tk.Canvas(window, width=200, height=100)
-    rect.pack()
-    rect.create_rectangle(50, 25, 150, 75, fill="blue")
-    rect.bind("<Button-1>", open_camera)  # Evento para hacer clic en el rectángulo
+    if not hasattr(window, 'rect'):
+        window.rect = tk.Canvas(window, width=800, height=600, bg='#5DADE2')
+        window.rect.create_rectangle(x1, y1, x2, y2, fill="#1B4F72")
+        window.rect.bind("<Button-1>", open_camera)
+    window.rect.pack(pady=(0,0))
 
 def open_camera(event):
     messagebox.showinfo("Info", "Abriría la cámara aquí")
     
-
-
 window = tk.Tk()
 window.title("Sistema de Login")
 
@@ -43,22 +50,22 @@ center_y = int((screen_height - window_height) / 2)
 
 window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-login_frame = tk.Frame(window)
-login_frame.place(relx=0.5, rely=0.5, anchor='center')
-
 background_image = tk.PhotoImage(file="fondo.png")
 background_label = tk.Label(window, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-tk.Label(window, text="Nombre de usuario").pack()
-entry_username = tk.Entry(window)
+login_frame = tk.Frame(window)
+login_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+tk.Label(login_frame, text="Nombre de usuario").pack()
+entry_username = tk.Entry(login_frame)
 entry_username.pack()
 
-tk.Label(window, text="Contraseña").pack()
-entry_password = tk.Entry(window, show="*")
+tk.Label(login_frame, text="Contraseña").pack()
+entry_password = tk.Entry(login_frame, show="*")
 entry_password.pack()
 
-button_login = tk.Button(window, text="Ingresar", command=login)
+button_login = tk.Button(login_frame, text="Ingresar", command=login)
 button_login.pack()
 
 window.mainloop()
